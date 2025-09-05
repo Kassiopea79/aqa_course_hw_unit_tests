3a3f6ec6246b6e983b02e5072b0b637b58117837
Kasiopea <kassiopeamaratik@gmail.com>
2025-09-04 21:34:19 -0400

    HW8: addede solutions for task 1

--- homework-solutions/js/hw-8/task-1.mjs
+++ homework-solutions/js/hw-8/task-1.mjs
@@ -14,13 +14,35 @@
 */
 const numbers = [7, 8, 2, 30, 85, 95, 77, 94, 37, 31];
 
-let forEach;
-let map;
-let filter;
-let find;
-let sort;
-let reduce;
-let some;
-let every;
+let forEach = [];
+numbers.forEach(function (n) {
+if (n % 3 === 0) {
+   forEach.push(n);
+}
+});
+let map = numbers.map(function (n) {
+   return n - numbers.length;
+});
+let filter = numbers.filter(function (n, i, arr) {
+   if (i > 0 && n > arr[i - 1]) {
+   return true;
+}
+   return false;
+});
+let find = numbers.find(function (n, i) {
+   return n === i;
+});
+let sort = numbers.slice().sort(function (a, b) {
+   return a - b;
+});
+let reduce = numbers.reduce(function (sum, n) {
+   return sum + n;
+}, 0);
+let some = numbers.some(function (n) {
+   return n > 90;
+});
+let every = numbers.every(function (n) {
+   return n >= 10 && n <= 99;
+});
 
 export { forEach, map, filter, find, sort, reduce, some, every };
