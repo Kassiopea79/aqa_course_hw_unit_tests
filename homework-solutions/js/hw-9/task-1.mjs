a234e6d32003efde0a3b2e9f3bf47007d2c589f9
Kasiopea <kassiopeamaratik@gmail.com>
2025-09-06 20:12:08 -0400

    HW9: completed task 1

--- homework-solutions/js/hw-9/task-1.mjs
+++ homework-solutions/js/hw-9/task-1.mjs
@@ -12,20 +12,39 @@ const character = { name: 'Barney', age: 36, gender: 'male', isQa: true };
 
 // 1
 let keyWithFourChars;
+keyWithFourChars = [];
+let keys = Object.keys(character);
+for (let i = 0; i < keys.length; i++) {
+  if (keys[i].length === 4) {
+    keyWithFourChars.push(keys[i]);
+  }
+}
 
 // 2
 let stringValues;
+stringValues = [];
+let values = Object.values(character);
+for (let i = 0; i < values.length; i++) {
+  if (typeof values[i] === 'string') {
+    stringValues.push(values[i]);
+  }
+}
 
 // 3
 /* Do not touch this part */
 const logSpy = jest.spyOn(console, 'log');
-// ---
-
-// Ваш код
 let keyValuePairs;
+keyValuePairs = Object.entries(character);
+for (let i = 0; i < keyValuePairs.length; i++) {
+  let key = keyValuePairs[i][0];
+  let value = keyValuePairs[i][1];
+  console.log(`key = ${key}, value = ${value}`);
+}
 
 // 4
 let hasSalaryKey1stOption;
+hasSalaryKey1stOption = 'salary' in character;
 let hasSalaryKey2ndOption;
+hasSalaryKey2ndOption = Object.hasOwn(character, 'salary');
 
 export { keyWithFourChars, stringValues, hasSalaryKey1stOption, hasSalaryKey2ndOption, keyValuePairs, logSpy };
